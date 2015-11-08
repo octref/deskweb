a6c2545391ea1a1c6043a2ad794c936f145785c8
Pine Wu <octref@gmail.com>
2015-11-07 20:58:01 -0500

    Clean up /site

--- site/main.js
+++ site/main.js
@@ -1,27 +1,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var $ = require('jquery');
+
 var InstagramWidget = React.createClass({
   getInitialState: function() {
-      return {
-          currentlist: this.props.newImageList,
-          index: 0
-      };
+    return {
+      currentlist: this.props.newImageList,
+      index: 0
+    };
   },
+
   componentWillReceiveProps: function(nextProps) {
-    // var newlist=[];
-    // for (i = 0; i < this.props.imageList.length; i++) {
-    //   if (!_.includes(this.state.currentlist, this.props.newImageList[i])){
-    //     newlist.push(this.props.newImageList[i]);
-    //   }
-    // }
     this.setState({
-        currentlist: nextProps.newImageList
+      currentlist: nextProps.newImageList
     });
   },
 
   switchImage: function() {
-    if (this.state.index !== (this.state.currentlist.length-1)) {
+    if (this.state.index !== (this.state.currentlist.length - 1)) {
 
       this.setState({
         index: this.state.index + 1, 
@@ -34,8 +31,16 @@ var InstagramWidget = React.createClass({
     }
   },
 
+  componentDidUpdate: function() {
+    $.post('http://localhost:4001', {
+      markup: document.documentElement.innerHTML
+    }, function() {
+      console.log('Sent update to bgserver');
+    });
+  },
+
   componentDidMount: function() {
-    this.interval = setInterval(this.switchImage, 1000);
+    this.interval = setInterval(this.switchImage, 5000);
   },
 
   componentWillUnmount: function() {
@@ -45,25 +50,13 @@ var InstagramWidget = React.createClass({
   render: function() {
     return (
       <img src={this.state.currentlist[this.state.index]} width="600" height="600"></img>
- 
     );
   }
 });
 
-// var i = 0;
-// var imageList = require('./imageList.json');
-// var list1 = imageList.slice(0, 4);
-// var list2 = imageList.slice(2, 6);
-// var list3 = imageList.slice(7, 11);
-// var list4 = imageList.slice(11, 15);
-// var lists = [list1, list2, list3, list4];
-var imageFeedHandler = function () {
-  var newImageList = require('./imageList.json');
-  ReactDOM.render(
-    <InstagramWidget newImageList= {newImageList} />,
-    document.getElementById('root')
-  );
-};
+var newImageList = require('./imageList.json');
 
-imageFeedHandler();
-setInterval(imageFeedHandler, 10000);
+ReactDOM.render(
+  <InstagramWidget newImageList={newImageList} />,
+  document.getElementById('root')
+);
